14000043434d1726af5aa4338e542e37b3372c55
MAG <albornoz.mateo.3059@eam.edu.co>
2026-05-20 19:43:22 -0500

    Agregue el perfil, que incluye la pagina del perfil del usuario con sus opciones de editar perfil funcionando toda completamente

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Registro from "./pages/Registro"
 import Login from "./pages/Login"
 import PanelAdmin from "./pages/PanelAdmin"
 import PanelAdminV2 from "./pages/PanelAdminV2"
+import Perfil from "./pages/Perfil"
 
 
 export default function App() {
@@ -19,6 +20,8 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/panel-admin" element={<PanelAdmin />} />
         <Route path="/panel-admin-v2" element={<PanelAdminV2 />} />
+
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
 
 
